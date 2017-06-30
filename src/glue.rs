d6bcc80c56aa4921ba6bd86192a21d717e102536
Petros Angelatos <petrosagg@gmail.com>
2017-06-29 23:47:18 -0700

    run clippy over the code
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- src/glue.rs
+++ src/glue.rs
@@ -5,9 +5,10 @@ use libc::{ENOENT, ERANGE};
 use std::ffi::CStr;
 use std::str;
 use std::mem::size_of;
+use std::ptr;
 
 #[no_mangle]
-pub extern "C" fn _nss_openvpn_gethostbyname2_r(
+pub unsafe extern "C" fn _nss_openvpn_gethostbyname2_r(
     name: *const c_char,
     af: i32,
     result: *mut hostent,
@@ -17,7 +18,7 @@ pub extern "C" fn _nss_openvpn_gethostbyname2_r(
     h_errnop: *mut i32,
 ) -> NssStatus {
     if af != AF_INET {
-        unsafe { *errnop = ENOENT; }
+        *errnop = ENOENT;
         return NssStatus::NotFound;
     }
 
@@ -31,7 +32,7 @@ struct HostData {
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_openvpn_gethostbyname_r(
+pub unsafe extern "C" fn _nss_openvpn_gethostbyname_r(
     name: *const c_char,
     result: *mut hostent,
     buffer: *mut c_char,
@@ -40,42 +41,40 @@ pub extern "C" fn _nss_openvpn_gethostbyname_r(
     h_errnop: *mut i32,
 ) -> NssStatus {
     assert!(!result.is_null() && !name.is_null() && !buffer.is_null());
-    unsafe { *h_errnop = 0; }
+    *h_errnop = 0;
 
     if buflen < size_of::<HostData>() {
-        unsafe { *errnop = ERANGE };
+        *errnop = ERANGE;
         return NssStatus::TryAgain;
     }
 
-    let name = match unsafe { CStr::from_ptr(name) }.to_str() {
+    let name = match CStr::from_ptr(name).to_str() {
         Ok(s) => s,
         Err(_) => {
-            unsafe { *errnop = ENOENT };
+            *errnop = ENOENT;
             return NssStatus::NotFound;
         }
     };
 
     match gethostbyname(name) {
         Ok(ip) => {
-            unsafe {
-                let mut data = buffer as *mut HostData;
-                (*result).h_addrtype = AF_INET;
-                (*result).h_length = size_of::<u32>() as i32;
-                (*result).h_aliases = 0 as *mut *mut i8;
+            let mut data = buffer as *mut HostData;
+            (*result).h_addrtype = AF_INET;
+            (*result).h_length = size_of::<u32>() as i32;
+            (*result).h_aliases = ptr::null_mut();
 
-                (*result).h_addr_list = (*data).addr_list.as_mut_ptr() as *mut *mut i8;
-                (*data).name[..name.len()].copy_from_slice(name.as_bytes());
-                (*data).name[name.len()] = 0;
-                (*data).s_addr = u32::from(ip).to_be();
-                (*data).addr_list[0] = &mut (*data).s_addr;
-                (*data).addr_list[1] = 0 as *mut u32;
-            }
+            (*result).h_addr_list = (*data).addr_list.as_mut_ptr() as *mut *mut i8;
+            (*data).name[..name.len()].copy_from_slice(name.as_bytes());
+            (*data).name[name.len()] = 0;
+            (*data).s_addr = u32::from(ip).to_be();
+            (*data).addr_list[0] = &mut (*data).s_addr;
+            (*data).addr_list[1] = ptr::null_mut();
 
-            return NssStatus::Success;
+            NssStatus::Success
         },
         Err(code) => {
-            unsafe { *errnop = ENOENT };
-            return code;
+            *errnop = ENOENT;
+            code
         }
     }
 }
